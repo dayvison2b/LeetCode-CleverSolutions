43e422d921ac1a063100ee0d7190c8778850d069
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-10-01 19:19:44 -0300

    solution updated

--- Leetcode/reverse-words-in-a-string-iii.py
+++ Leetcode/reverse-words-in-a-string-iii.py
@@ -6,3 +6,9 @@ class Solution:
         for word in s.split():
             result.append(word[::-1])
         return " ".join(result)
+    
+    
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join([word[::-1] for word in s.split()])
+    
