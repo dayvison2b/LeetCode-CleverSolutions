99dd81992d0cf031c6d3be753ff480229a4d3447
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-10-02 14:42:58 -0300

    remove-colored-pieces-if-both-neighbors-are-the-same-color solution updated

--- Leetcode/remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ Leetcode/remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -9,4 +9,14 @@ class Solution:
             elif colors[i-1] == colors[i+1] == colors[i] == "B":
                 b_count += 1
         return a_count > b_count
-    
+
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        a_count,b_count = 0,0
+        for i in range(1, len(colors) - 1):
+            if colors[i-1] == colors[i] == colors[i+1]:
+                if colors[i] == "A":
+                    a_count += 1
+                else:
+                    b_count += 1
+        return a_count > b_count
