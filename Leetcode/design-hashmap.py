7e7c6b6a3844c72378ae93db39da831c19274606
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-10-04 21:23:02 -0300

    design-hashmap solution updated

--- Leetcode/design-hashmap.py
+++ Leetcode/design-hashmap.py
@@ -1,6 +1,50 @@
 # https://leetcode.com/problems/design-hashmap/?envType=daily-question&envId=2023-10-04
 
 class MyHashMap:
+    def __init__(self):
+        self.size = 10
+        self.mp = [[] for _ in range(self.size)]
+
+    def hash(self, key):
+        return key % self.size
+
+    def put(self, key, value):
+        i = self.hash(key)
+        bucket = self.mp[i]
+
+        for idx, pair in enumerate(bucket):
+            if pair[0] == key:
+                bucket[idx] = (key, value)
+                return
+
+        bucket.append((key, value))
+
+    def get(self, key):
+        i = self.hash(key)
+        bucket = self.mp[i]
+
+        for pair in bucket:
+            if pair[0] == key:
+                return pair[1]
+
+        return -1
+
+    def remove(self, key):
+        i = self.hash(key)
+        bucket = self.mp[i]
+
+        for idx, pair in enumerate(bucket):
+            if pair[0] == key:
+                del bucket[idx]
+                return
+            
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+            
+class MyHashMap:
 
     def __init__(self):
         self.hashKey = []
@@ -22,11 +66,6 @@ class MyHashMap:
             self.hashKey.pop(idx)
             self.hashVal.pop(idx)
 
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
 
 class MyHashMap:
 
