7a731793ea16a6c374f219c32d70ed91348f6b1a
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-10-08 18:57:05 -0300

    solution updated

--- Leetcode/max-dot-product-of-two-subsequences.py
+++ Leetcode/max-dot-product-of-two-subsequences.py
@@ -2,8 +2,7 @@
 
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        n = len(nums1)
-        m = len(nums2)
+        n,m = len(nums1),len(nums2)
         dp = [[-10] * (m + 1) for _ in range(n + 1)]
 
         for i in range(1, n + 1):
