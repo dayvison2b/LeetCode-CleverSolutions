f4d22e269310353b4fe1904b46cda06399d64350
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-10-10 15:45:06 -0300

    minimum-number-of-operations-to-make-array-continuous solution updated

---
+++ Leetcode/minimum-number-of-operations-to-make-array-continuous.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous/
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        N = len(nums)
+        queue = deque()
+        max_length = 1
+
+        for num in sorted(set(nums)):
+            while queue and num - queue[0] >= N:
+                queue.popleft()
+
+            queue.append(num)
+            max_length = max(max_length, len(queue))
+
+        return N - max_length
