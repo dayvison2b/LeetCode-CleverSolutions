b8846ca64866422695dc877534027075eb332eae
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-10-13 20:50:06 -0300

    min-cost-climbing-stairs solution updated

--- Leetcode/min-cost-climbing-stairs.py
+++ Leetcode/min-cost-climbing-stairs.py
@@ -10,3 +10,15 @@ class Solution:
             memo1 = memo2
             memo2 = current
         return min(memo1,memo2)
+
+class Solution:
+    def minCostClimbingStairs(self, cost):
+        n = len(cost)
+        dp = [0] * n
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        
+        return min(dp[n-1], dp[n-2])
